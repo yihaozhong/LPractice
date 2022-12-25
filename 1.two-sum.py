7bb20c4fa09193836f3116a7f39a186aa7fb63aa
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2022-12-24 23:33:21 -0800

    use HashMap

---
+++ 1.two-sum.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=1 lang=python
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i, ele in enumerate(nums):
+            comp_ele = target - ele
+            if comp_ele in dict:
+                return [dict[comp_ele], i]
+            else:
+                dict[ele] = i
+
+    
+        
+# @lc code=end
+
