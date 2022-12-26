72df314231180ed566571828bf7b9e68385595c3
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2022-12-26 11:59:13 -0800

    two pointer [merge]

---
+++ 21.merge-two-sorted-lists.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=21 lang=python
+#
+# [21] Merge Two Sorted Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        prehead = ListNode(-1)
+        prev = prehead
+        while list1 and list2:
+            if list1.val < list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+        # At least one of l1 and l2 can still have nodes at this point, so connect
+        # the non-null list to the end of the merged list
+        prev.next = list2 if list2 is not None else list1
+
+        return prehead.next
+
+        
+
+# @lc code=end
+
