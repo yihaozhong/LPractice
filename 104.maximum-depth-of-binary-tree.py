f7c9ef226ddd18575d969ee9fde28a7580f87ca2
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2022-12-27 17:21:42 -0800

    binary tree [depth of tree traverse]

---
+++ 104.maximum-depth-of-binary-tree.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=104 lang=python3
+#
+# [104] Maximum Depth of Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    res = 0
+    curDepth = 0
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def traverse(root):
+            # base
+            if(root is None):
+                return 
+            
+            # pre-order
+            self.curDepth += 1
+
+                # if this is the end leaf
+            if (root.left is None) and (root.right is None):
+                self.res = max(self.res, self.curDepth)
+            
+            traverse(root.left)
+            traverse(root.right)
+
+            # post-order
+            self.curDepth -= 1
+        
+        traverse(root)
+
+        return self.res
+
+    # O(N)
+    # O(1)
+        
+# @lc code=end
+
