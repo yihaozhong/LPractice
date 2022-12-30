a48e4ce289d2613e88549947ec6ed6eb0b668a8d
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2022-12-29 17:59:31 -0800

    preSum [array]

---
+++ 303.range-sum-query-immutable.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=303 lang=python3
+#
+# [303] Range Sum Query - Immutable
+#
+
+# @lc code=start
+class NumArray:
+    preSum = None
+
+    def __init__(self, nums: List[int]):
+        self.preSum = [0] * (len(nums)+1)
+        for i in range(1, len(self.preSum)):
+            self.preSum[i] = (self.preSum[i-1] + nums[i-1])
+        return None
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.preSum[right+1] - self.preSum[left]
+
+    # O(1) per queries, O(N) pre-computation
+    # O(n) space
+    
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
+# @lc code=end
+
