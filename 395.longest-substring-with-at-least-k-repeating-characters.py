1ebad472445df815af12d43a04cbbe8df088083c
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-01-09 23:18:41 -0500

    string

---
+++ 395.longest-substring-with-at-least-k-repeating-characters.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=395 lang=python3
+#
+# [395] Longest Substring with At Least K Repeating Characters
+#
+
+# @lc code=start
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+        
+# @lc code=end
+
