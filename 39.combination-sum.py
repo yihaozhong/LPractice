f6c8d6f94f4681c77bf8ac7b51a8be765b04d1b9
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-01-26 16:32:28 -0500

    combination backtracking

---
+++ 39.combination-sum.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=39 lang=python3
+#
+# [39] Combination Sum
+#
+
+# @lc code=start
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        results = []
+
+        def backtrack(remain, comb, start):
+            if remain == 0:
+                # make a deep copy of the current combination
+                results.append(list(comb))
+                return
+            elif remain < 0:
+                # exceed the scope, stop exploration.
+                return
+
+            for i in range(start, len(candidates)):
+                # add the number into the combination
+                comb.append(candidates[i])
+                # give the current number another chance, rather than moving on
+                backtrack(remain - candidates[i], comb, i)
+                # backtrack, remove the number from the combination
+                comb.pop()
+
+        backtrack(target, [], 0)
+
+        return results
+
+
+# @lc code=end
+
