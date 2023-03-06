19cf6b842957f0f44b27c1f0c563f9eb76c107e5
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-03-05 20:45:15 -0500

    array - group dict

--- 49.group-anagrams.py
+++ 49.group-anagrams.py
@@ -14,28 +14,28 @@ class Solution:
         for c in s:
             delta = ord(c) - ord('a')
             count[delta] += 1
-        return ''.join(str(x) for x in count)
+        return tuple(count) #''.join(str(x) for x in count)
 
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # dict = {}
-        # for i in strs:
-        #     code = self.encode(i)
-        #     if code not in dict:
-        #         dict[code] = []
-        #     dict[code].append(i)
+        dict = {}
+        for i in strs:
+            code = self.encode(i)
+            if code not in dict:
+                dict[code] = []
+            dict[code].append(i)
 
-        # res = []
-        # for g in dict.values():
-        #     res.append(g)
-        # return res
+        res = []
+        for g in dict.values():
+            res.append(g)
+        return res
         
-        ans = collections.defaultdict(list)
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            ans[tuple(count)].append(s)
-        return ans.values()
+        # ans = collections.defaultdict(list)
+        # for s in strs:
+        #     count = [0] * 26
+        #     for c in s:
+        #         count[ord(c) - ord('a')] += 1
+        #     ans[tuple(count)].append(s)
+        # return ans.values()
 
 # @lc code=end
 
