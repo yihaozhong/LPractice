864b64a45983a194f964c7b18364a8f97a969fd7
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-03-06 20:40:30 -0500

    two pointer

---
+++ 125.valid-palindrome.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
+                return False
+
+            i += 1
+            j -= 1
+
+        return True
+# @lc code=end
+
