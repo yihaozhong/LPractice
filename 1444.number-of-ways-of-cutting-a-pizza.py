fb4814998f2076ffde25a1d389a34cbffbfe27fe
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-03-31 18:34:31 -0400

    dfs

---
+++ 1444.number-of-ways-of-cutting-a-pizza.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1444 lang=python3
+#
+# [1444] Number of Ways of Cutting a Pizza
+#
+
+# @lc code=start
+class Solution:
+    def ways(self, pizza: List[str], k: int) -> int:
+        
+# @lc code=end
+
