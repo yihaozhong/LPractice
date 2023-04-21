58331472e53927c63f559a12822385e55e4074d1
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-04-21 17:45:25 -0400

    update binary search

--- 704.binary-search.py
+++ 704.binary-search.py
@@ -8,17 +8,31 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # classic binary search
-        left = 0
-        right = len(nums) - 1
+        # left = 0
+        # right = len(nums) - 1
 
-        while (left <= right):
-            mid = (left + right)//2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
+        # while (left <= right):
+        #     mid = (left + right)//2
+        #     if nums[mid] == target:
+        #         return mid
+        #     elif nums[mid] < target:
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
+        # return -1
+
+        # rewrite 
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
                 left = mid + 1
-            else:
+            elif nums[mid] > target:
                 right = mid - 1
-        return -1
+            else:
+                return mid 
+        return -1 
+    
 # @lc code=end
 
