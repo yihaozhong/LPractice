2cc1697c4917ef4cfc5ee27d7ce4e36961f86c93
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-04-21 23:13:53 -0400

    binary search

--- 34.find-first-and-last-position-of-element-in-sorted-array.py
+++ 34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -7,37 +7,65 @@
 # @lc code=start
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # def left_bound(nums, target):
+        #     left = 0
+        #     right = len(nums) -1
+        #     while (left <= right):
+        #         mid = (left + right)//2
+        #         if (nums[mid] == target):
+        #             right = mid -1
+        #         elif (nums[mid] < target):
+        #             left = mid + 1
+        #         elif (nums[mid] > target):
+        #             right = mid - 1
+        #     if (left >= len(nums) or nums[left]!= target):
+        #         return -1
+        #     return left # left == right here
+
+        # def right_bound(nums, target):
+        #     left = 0
+        #     right = len(nums) -1
+        #     while (left <= right):
+        #         mid = (left + right)//2
+        #         if (nums[mid] == target):
+        #             left = mid +1
+        #         elif (nums[mid] < target):
+        #             left = mid + 1
+        #         elif (nums[mid] > target):
+        #             right = mid - 1
+        #     if (right < 0 or nums[right]!= target):
+        #         return -1
+        #     return right # left == right here
+
+        # return [left_bound(nums, target), right_bound(nums, target)]
+
         def left_bound(nums, target):
-            left = 0
-            right = len(nums) -1
-            while (left <= right):
-                mid = (left + right)//2
-                if (nums[mid] == target):
-                    right = mid -1
-                elif (nums[mid] < target):
+            left, right = 0, len(nums) - 1
+            left_index = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    left_index = mid 
+                    right = mid- 1
+                elif nums[mid] > target:
+                    right = mid- 1
+                else:
                     left = mid + 1
-                elif (nums[mid] > target):
-                    right = mid - 1
-            if (left >= len(nums) or nums[left]!= target):
-                return -1
-            return left # left == right here
-
-        def right_bound(nums, target):
-            left = 0
-            right = len(nums) -1
-            while (left <= right):
-                mid = (left + right)//2
-                if (nums[mid] == target):
-                    left = mid +1
-                elif (nums[mid] < target):
-                    left = mid + 1
-                elif (nums[mid] > target):
-                    right = mid - 1
-            if (right < 0 or nums[right]!= target):
-                return -1
-            return right # left == right here
-
-        return [left_bound(nums, target), right_bound(nums, target)]
+            return left_index
         
+        def right_bound(nums, target):
+            left, right = 0, len(nums) - 1
+            right_index = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] <= target:
+                    left = mid + 1
+                    if nums[mid] == target:
+                        right_index = mid
+                else:
+                    right = mid - 1
+            return right_index
+        return [left_bound(nums, target), right_bound(nums, target)]
+
 # @lc code=end
 
