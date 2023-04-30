c0f326cba3926835f5493fbcc7a20b43329a6f00
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-04-29 21:39:24 -0400

    dfs

---
+++ 98.validate-binary-search-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=98 lang=python3
+#
+# [98] Validate Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def traverse(root, minVal, maxVal):
+            if root is None:
+                return True
+            
+            if root.val >= maxVal or root.val <= minVal:
+                return False
+            return traverse(root.left, minVal, root.val) and traverse(root.right, root.val, maxVal)
+        return traverse(root, float('-inf'), float('inf'))
+    
+        # O(N)
+        # O(N)
+
+        
+# @lc code=end
+
