8e355b767e981d9ab9f59428694e09c3f2577db1
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-04-30 00:10:20 -0400

    dfs

---
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,47 @@
+# @before-stub-for-debug-begin
+from python3problem235 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode id=235 lang=python3
+#
+# [235] Lowest Common Ancestor of a Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def traverse(root: 'TreeNode', le: int, ri: int):
+            # in order traversal 
+            
+            if root.val < le:
+                
+                return traverse(root.right, le, ri)
+            
+            if le < root.val < ri:
+                
+                return root
+
+            elif root.val > ri:
+                return traverse(root.left, le, ri)
+
+            return root
+        
+        if p.val < q.val:
+            left = p.val
+            right = q.val
+        else:
+            left = q.val
+            right = p.val
+
+        return traverse(root, left, right)
+# @lc code=end
+
