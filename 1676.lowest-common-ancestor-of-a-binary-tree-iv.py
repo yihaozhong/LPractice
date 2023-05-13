d18056e0a7d65f447f80f4773c300473acdfb9e1
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-05-12 20:55:39 -0400

    dfs

---
+++ 1676.lowest-common-ancestor-of-a-binary-tree-iv.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=1676 lang=python3
+#
+# [1676] Lowest Common Ancestor of a Binary Tree IV
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
+        
+# @lc code=end
+
