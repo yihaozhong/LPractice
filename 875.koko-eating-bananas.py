75824bdbe6140206c317b16a4b6b9384f2e586f6
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-05-16 18:24:14 -0400

    bs

--- 875.koko-eating-bananas.py
+++ 875.koko-eating-bananas.py
@@ -7,6 +7,23 @@
 # @lc code=start
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        
-# @lc code=end
+        def feasible(speed):
+            return sum(math.ceil(pile / speed) for pile in piles) <= h  # slower
+            # return sum((pile - 1) / speed + 1 for pile in piles) <= h  # faster
+         # Iterate over the piles and calculate hour_spent.
+            # We increase the hour_spent by ceil(pile / middle)
+        # for pile in piles:
+        #     hour_spent += math.ceil(pile / middle)
 
+        left, right = 1, max(piles)
+        while left < right:
+            mid = left + (right - left) // 2
+            if feasible(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+    
+    #  The initial search space is from 1 to m, 
+    #  it takes logm comparisons to reduce the search space to 1.
+# @lc code=end
