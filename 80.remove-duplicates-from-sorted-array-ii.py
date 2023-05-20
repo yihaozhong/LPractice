12782eccc3332630440e784ce119abeecd20a0b8
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-05-20 16:12:22 -0400

    array

--- 80.remove-duplicates-from-sorted-array-ii.py
+++ 80.remove-duplicates-from-sorted-array-ii.py
@@ -23,5 +23,37 @@ class Solution:
             if fast <len(nums) and nums[fast] != nums[fast - 1]:
                 count = 0
         return slow+1
+    
+    # sol 2
+
+    # def removeDuplicates(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+        
+    #     # Initialize the counter and the second pointer.
+    #     j, count = 1, 1
+        
+    #     # Start from the second element of the array and process
+    #     # elements one by one.
+    #     for i in range(1, len(nums)):
+            
+    #         # If the current element is a duplicate, 
+    #         # increment the count.
+    #         if nums[i] == nums[i - 1]:
+    #             count += 1
+    #         else:
+    #             # Reset the count since we encountered a different element
+    #             # than the previous one
+    #             count = 1
+            
+    #         # For a count <= 2, we copy the element over thus
+    #         # overwriting the element at index "j" in the array
+    #         if count <= 2:
+    #             nums[j] = nums[i]
+    #             j += 1
+                
+    #     return j
 # @lc code=end
 
