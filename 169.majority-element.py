d771387b68e6973adaa620c82ffeba972b1cd9b9
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-05-20 16:52:30 -0400

    array

--- 169.majority-element.py
+++ 169.majority-element.py
@@ -7,14 +7,32 @@
 # @lc code=start
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        result = dict()
-        for i in nums:
-            if i in result:
-                result[i] += 1
-            else:
-                result[i] = 1
-            if result[i] > len(nums)//2:
-                    return i
+        # result = dict()
+        # for i in nums:
+        #     if i in result:
+        #         result[i] += 1
+        #     else:
+        #         result[i] = 1
+        #     if result[i] > len(nums)//2:
+        #             return i
         
+        # sol 2 Time complexity : O(nlgn)
+        # nums.sort() 
+        # return nums[len(nums)//2]
+
+
+        # sol 3
+        # counts = collections.Counter(nums)
+        # return max(counts.keys(), key=counts.get)
+
+        # Need O(1) space
+        curr, count = nums[0], 1              # curr will store the current majority element, count will store the count of the majority
+        for i in range(1,len(nums)):
+            count += (1 if curr == nums[i] else -1)    # if i is equal to current majority, they're in same team, hence added, else one current majority and i both will be dead
+            if not count:                   # if count is 0 means King is de-throwned
+                curr = nums[i+1] if i + 1 < len(nums) else None        # the next element is the new King
+                count = 0         # starting it with 0 because we can't increment the i of the for loop, the count will be 1 in next iteration, and again the battle continues after next iteration
+        return curr # Boyer-Moore Voting Algorithm
+
 # @lc code=end
 
