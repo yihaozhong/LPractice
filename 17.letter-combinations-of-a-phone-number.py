55f51c2e63c598d4e9ef56cec12beb46dc9f7bd3
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-05-22 17:59:23 -0400

    backtracking

---
+++ 17.letter-combinations-of-a-phone-number.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=17 lang=python3
+#
+# [17] Letter Combinations of a Phone Number
+#
+
+# @lc code=start
+class Solution:
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        KEYBOARD = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+
+        def dfs(start_index, path):
+            if start_index == len(digits):
+                result.append(''.join(path))
+                return
+            nextNumber = digits[start_index]
+            for letter in KEYBOARD[nextNumber]:
+                path.append(letter)
+                dfs(start_index+1, path)
+                path.pop()
+
+        result = []
+        if digits == "":
+            return result
+        dfs(0, [])
+        return result
+
+# @lc code=end
