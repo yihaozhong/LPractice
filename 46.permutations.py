72833b07c33115f23c2226b14cadb1de9f6c949a
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-05-22 19:27:11 -0400

    backtracking

---
+++ 46.permutations.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=46 lang=python3
+#
+# [46] Permutations
+#
+
+"""
+
+# iterate all possible candidates.
+    for next_candidate in list_of_candidates:
+        if is_valid(next_candidate):
+            # try this partial candidate solution
+            place(next_candidate)
+            # given the candidate, explore further.
+            backtrack(next_candidate)
+            # backtrack
+            remove(next_candidate)
+"""
+
+
+# @lc code=start
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def dfs(start, path, used):
+            if start == len(nums):
+                result.append(path[:])
+                return
+            for i, num in enumerate(nums):
+                if not used[i]:
+                    path.append(num)
+                    used[i] = True
+                    dfs(start+1, path, used)
+                    # remove letter from permutation, mark letter as unused
+                    path.pop()
+                    used[i] = False
+
+        result, use = [], [False] * len(nums)
+        dfs(0, [], use)
+        return result
+# O(N!)
+# @lc code=end
+
