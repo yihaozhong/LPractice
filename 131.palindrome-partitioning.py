aad6d712213f38fe2ccff4820f3ee0e95d2d2e1a
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-05-22 18:16:28 -0400

    backtracking

--- 131.palindrome-partitioning.py
+++ 131.palindrome-partitioning.py
@@ -7,6 +7,28 @@
 # @lc code=start
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        
-# @lc code=end
+        def check(s: str):
+            first, last = 0, len(s) - 1
+            while first < last:
+                if s[first] != s[last]:
+                    return False
+                first += 1
+                last -= 1
+            return True
 
+        def dfs(start_index, path):
+            if start_index == len(s):
+                result.append(path)
+                return
+            for end in range(start_index + 1, len(s) + 1):
+                if check(s[start_index: end]):
+                    dfs(end, path + [s[start_index: end]])
+
+        result = []
+        dfs(0, [])
+        return result
+    
+    #  For each letter in the input string, we can either include it as a previous string or start a new string with it. With those two choices, the total number of operations is 2^n. 
+    #  We also need O(n) to check if the string is a palindrome.
+    # O(2^N*N) 
+# @lc code=end
