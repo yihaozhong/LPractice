523f61f4e2b16b925a8be6c1715f20e3d47e3829
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-05-22 19:46:29 -0400

    backtracking

--- 46.permutations.py
+++ 46.permutations.py
@@ -37,6 +37,28 @@ class Solution:
         result, use = [], [False] * len(nums)
         dfs(0, [], use)
         return result
+
 # O(N!)
+
+"""
+def backtrack(first = 0):
+            # if all integers are used up
+            if first == n:  
+                output.append(nums[:])
+            for i in range(first, n):
+                # place i-th integer first 
+                # in the current permutation
+                nums[first], nums[i] = nums[i], nums[first]
+                # use next integers to complete the permutations
+                backtrack(first + 1)
+                # backtrack
+                nums[first], nums[i] = nums[i], nums[first]
+        
+        n = len(nums)
+        output = []
+        backtrack()
+        return output
+
+"""
 # @lc code=end
 
