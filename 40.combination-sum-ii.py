096db75c8a3b5bd0e26d661d61b7aebae75eb197
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-05-26 16:44:07 -0400

    backtracking

--- 40.combination-sum-ii.py
+++ 40.combination-sum-ii.py
@@ -12,43 +12,68 @@ The idea is that it incrementally builds candidates to the solutions, and abando
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         # backtracking with counter 
-        def backtrack(comb, remain, curr, counter, results):
-            if remain == 0:
-                # make a deep copy of the current combination
-                #   rather than keeping the reference.
-                results.append(list(comb))
-                return
-            elif remain < 0:
-                return
+        # def backtrack(comb, remain, curr, counter, results):
+        #     if remain == 0:
+        #         # make a deep copy of the current combination
+        #         #   rather than keeping the reference.
+        #         results.append(list(comb))
+        #         return
+        #     elif remain < 0:
+        #         return
 
-            for next_curr in range(curr, len(counter)):
-                candidate, freq = counter[next_curr]
+        #     for next_curr in range(curr, len(counter)):
+        #         candidate, freq = counter[next_curr]
 
-                if freq <= 0:
-                    continue
+        #         if freq <= 0:
+        #             continue
 
-                # add a new element to the current combination
-                comb.append(candidate)
-                counter[next_curr] = (candidate, freq-1)
+        #         # add a new element to the current combination
+        #         comb.append(candidate)
+        #         counter[next_curr] = (candidate, freq-1)
 
-                # continue the exploration with the updated combination
-                backtrack(comb, remain - candidate, next_curr, counter, results)
+        #         # continue the exploration with the updated combination
+        #         backtrack(comb, remain - candidate, next_curr, counter, results)
 
-                # backtrack the changes, so that we can try another candidate
-                counter[next_curr] = (candidate, freq)
-                comb.pop()
+        #         # backtrack the changes, so that we can try another candidate
+        #         counter[next_curr] = (candidate, freq)
+        #         comb.pop()
 
-        results = []  # container to hold the final combinations
-        counter = Counter(candidates)
-        # convert the counter table to a list of (num, count) tuples
-        counter = [(c, counter[c]) for c in counter]
+        # results = []  # container to hold the final combinations
+        # counter = Counter(candidates)
+        # # convert the counter table to a list of (num, count) tuples
+        # counter = [(c, counter[c]) for c in counter]
 
-        backtrack(comb = [], remain = target, curr = 0,
-                counter = counter, results = results)
+        # backtrack(comb = [], remain = target, curr = 0,
+        #         counter = counter, results = results)
 
-        return results
+        # return results
     
 
     # we can also do backtracking with index, just as 90 subset ii dedup
+        results = []
+
+        def backtrack(remain, comb, start):
+            if remain == 0:
+                # make a deep copy of the current combination
+                results.append(list(comb))
+                return
+            elif remain < 0:
+                # exceed the scope, stop exploration.
+                return
+
+            for i in range(start, len(candidates)):
+                if candidates[i - 1] == candidates[i] and i > start:
+                    continue
+                # add the number into the combination
+                comb.append(candidates[i])
+                # give the current number another chance, rather than moving on
+                backtrack(remain - candidates[i], comb, i+1)
+                # backtrack, remove the number from the combination
+                comb.pop()
+
+        candidates.sort()
+        backtrack(target, [], 0)
+
+        return results
 # @lc code=end
 
--- 47.permutations-ii.py
+++ 47.permutations-ii.py
@@ -15,7 +15,7 @@ class Solution:
             for i in range(len(nums)):
                 if used[i]:
                     continue
-                if i > 0 and nums[i] == nums[i-1] and not used[i-1]: # i > 0 to avoid 111, 222
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]: # i > 0 to avoid 111, 222, equal to i > start
                     # 如果前面的相邻相等元素没有用过，则跳过
                     continue
                 used[i] = True
