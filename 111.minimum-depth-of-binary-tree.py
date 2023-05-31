5d41e3f11f74cb848fae429cac35a8476e3a42fc
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-05-31 15:20:02 -0400

    dfs bfs

--- 111.minimum-depth-of-binary-tree.py
+++ 111.minimum-depth-of-binary-tree.py
@@ -14,27 +14,47 @@
 from queue import Queue
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return  0
-        q = Queue()
-        q.put(root)
-        # initialize the depth
-        depth = 1
+        # bfs
 
-        while((q.empty() == False)):
-            sz = q.qsize()
+        # if root is None:
+        #     return  0
+        # q = Queue()
+        # q.put(root)
+        # # initialize the depth
+        # depth = 1
 
-            # iterate every node in this level
-            for i in range(sz):
-                cur = q.get()
-                if cur.left is None and cur.right is None:
-                    return depth
-                if cur.left is not None:
-                    q.put(cur.left)
-                if cur.right is not None:
-                    q.put(cur.right)
-            depth += 1
-        return depth
+        # while((q.empty() == False)):
+        #     sz = q.qsize()
+
+        #     # iterate every node in this level
+        #     for i in range(sz):
+        #         cur = q.get()
+        #         if cur.left is None and cur.right is None:
+        #             return depth
+        #         if cur.left is not None:
+        #             q.put(cur.left)
+        #         if cur.right is not None:
+        #             q.put(cur.right)
+        #     depth += 1
+        # return depth
+
+        # dfs
+        res = float("inf")
+        def dfs(node = root, level = 0):
+            nonlocal res
+            if not node:
+                res = min(res, level)
+                return root
+            if node.left is None:
+                dfs(node.right, level + 1)
+            elif node.right is None:
+                dfs(node.left, level + 1)
+            else:
+                dfs(node.left, level + 1)
+                dfs(node.right, level + 1)
+
+        dfs()
+        return res
         
 # @lc code=end
 
