9a3eb6ed4b3e5689df1585cbc434b6b77c415732
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-06-27 21:42:52 -0400

    graph dfs

--- 547.number-of-provinces.py
+++ 547.number-of-provinces.py
@@ -7,6 +7,19 @@
 # @lc code=start
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        
+        N = len(isConnected)
+        seen = set()
+        def dfs(node):
+            for nei, adj in enumerate(isConnected[node]):
+                if adj and nei not in seen:
+                    seen.add(nei)
+                    dfs(nei)
+    
+        ans = 0
+        for i in range(N):
+            if i not in seen:
+                dfs(i)
+                ans += 1
+        return ans
 # @lc code=end
 
