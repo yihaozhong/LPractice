42f2a83378154b4d9d54e3496ca0f1592c5d6078
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-08-01 23:08:33 -0400

    linked list two pointer

--- 160.intersection-of-two-linked-lists.py
+++ 160.intersection-of-two-linked-lists.py
@@ -67,5 +67,22 @@ class Solution(object):
         p2 = p2.next
     return p1
     '''
+
+    '''
+    nodes_in_B = set()
+
+        while headB is not None:
+            nodes_in_B.add(headB)
+            headB = headB.next
+
+        while headA is not None:
+            # if we find the node pointed to by headA,
+            # in our set containing nodes of B, then return the node
+            if headA in nodes_in_B:
+                return headA
+            headA = headA.next
+
+        return None
+    '''
 # @lc code=end
 
