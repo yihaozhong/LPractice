874fdd54df1962dd46ec74a4c69f1ac6208ea777
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2024-05-25 21:29:58 -0700

    two pointer

--- 16.3-sum-closest.py
+++ 16.3-sum-closest.py
@@ -7,26 +7,28 @@
 # @lc code=start
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        def twoSumClosest(start, nums, target):
-            closest = float("inf")
-            left, right = start, len(nums) - 1
-            while(left < right):
-                if abs(closest) > abs(target - (nums[left] + nums[right])):
-                    closest = target - (nums[left] + nums[right])
-                if (nums[left] + nums[right]) < target:
+        def twoSumClosest(i, nums, target):
+            left, right = i, len(nums) - 1
+            delta = float('inf')
+            while left < right:
+                sum_ = nums[left] + nums[right]
+                if abs(delta) > abs(target - sum_):
+                    delta = target - sum_
+                if sum_ < target:
                     left += 1
                 else:
                     right -= 1
-            return target - closest
+            return target - delta
+        
         
         delta = float("inf")
         nums.sort()
         # 固定 nums[i] 为三数之和中的第一个数，
         # 然后对 nums[i+1..] 搜索接近 target - nums[i] 的两数之和
         for i in range(len(nums)- 2):
-            sum= twoSumClosest(i+1, nums, target - nums[i])+nums[i]
-            if abs(delta) > abs(target - sum):
-                delta = target - sum
+            sum_ = twoSumClosest(i+1, nums, target - nums[i])+nums[i]
+            if abs(delta) > abs(target - sum_):
+                delta = target - sum_
         return target - delta      
 # @lc code=end
 
